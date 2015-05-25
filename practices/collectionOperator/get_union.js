edd8ffce0c436b7925653a6978e8c1739ec5b0a3
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 13:55:46 +0000

    add practices/collectionOperator/get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,7 +2,9 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  var _ = require('../../lodash');
+  var result = _.difference(collection_b, collection_a);
+  result = collection_a.concat(result);
 }
 
 module.exports = get_union;
-

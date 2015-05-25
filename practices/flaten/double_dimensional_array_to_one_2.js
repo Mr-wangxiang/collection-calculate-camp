c3cc8708db833508b1f9a7bd38cabae5c965cf79
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 15:01:14 +0000

    add practices/flaten/double_dimensional_array_to_one_2.js

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,10 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var _ = require('../../lodash');
+  var result = _.flattenDeep(collection);
+  result = _.uniq(result);
+  return result;
 }
 
 module.exports = double_to_one;

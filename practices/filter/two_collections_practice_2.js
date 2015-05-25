f4c083c77595493d8ceb613a9a47a64aa9ea2df7
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 12:51:57 +0000

    add practice_2

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -6,7 +6,7 @@ function choose_no_common_elements(collection_a, collection_b) {
   var result = [];
   for (var i = 0; i < collection_a.length; i++) {
       for (var j = 0; j < collection_b.length; j++) {
-          if(collection_a[i] !=== collection_b[j]) {
+          if(collection_a[i] !== collection_b[j]) {
               result.push(collection_a[i]);
           }
       }

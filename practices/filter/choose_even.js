2599cc59712065beefd941ff75178188780735fe
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 13:59:36 +0000

    add practices/filter/choose_even.js

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,13 +3,9 @@
 function choose_even(collection) {
 
   //在这里写入代码
-  var result = [];
-  for (var i = 0; i < collection.length; i++) {
-      if(collection[i]%2 == 0) {
-          result.push(collection[i]);
-      }
-    }
-    return result;
+  var _ = require('../../lodash');
+  var result = _.remove(collection, function(n) { return n % 2 == 0; });
+  return result;
 }
 
 module.exports = choose_even;

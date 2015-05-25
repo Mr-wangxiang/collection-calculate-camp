2cd030ee1698c3b6659211eca44d89e5dfbe3ab8
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 10:55:49 +0000

    add practices/filter/choose_no_repeat_number.js

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,6 +3,14 @@
 function choose_no_repeat_number(collection) {
 
   //在这里写入代码
+  var result = []; //一个新的临时数组
+  for (var i = 0; i < collection.length; i++) {
+
+    if (result.indexOf(collection[i]) == -1)
+
+		result.push(collection[i]);
+  }
+  return result;
 }
 
 module.exports = choose_no_repeat_number;

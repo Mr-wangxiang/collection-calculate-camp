87cbc7d3c5cbb2a7ace57e0a4cd36dfff7f9c42c
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 14:33:30 +0000

    practices/filter/two_collections_practice_2.js

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,15 +3,17 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
-  var result = [];
-  for (var i = 0; i < collection_a.length; i++) {
-      for (var j = 0; j < collection_b.length; j++) {
-          if(collection_a[i] !== collection_b[j]) {
-              result.push(collection_a[i]);
-          }
-      }
-  }
-  return result;
+  // var result = [];
+  // for (var i = 0; i < collection_a.length; i++) {
+  //     for (var j = 0; j < collection_b.length; j++) {
+  //         if(collection_a[i] !== collection_b[j]) {
+  //             result.push(collection_a[i]);
+  //         }
+  //     }
+  // }
+  // return result;
+  var result = _.xor(collection_a, collection_b);
+  return result ;
 }
 
 module.exports = choose_no_common_elements;

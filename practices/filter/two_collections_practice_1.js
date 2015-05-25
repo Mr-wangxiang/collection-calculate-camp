86559c1a05d79762f73c9e5a6241cf481e5d6d72
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 13:10:15 +0000

    add practice_1

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -3,7 +3,7 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
-  var _ = require('../lodash');
+  var _ = require('lodash');
   var result = [];
   result = _.intersection(collection_a,collection_b);
   return result;

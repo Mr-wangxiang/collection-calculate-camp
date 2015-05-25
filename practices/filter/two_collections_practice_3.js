7e1e221d5f8360a674a0eb3d638bd4fa97045019
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 14:50:09 +0000

    add practices/flaten/double_dimensional_array_to_one_1.js

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,9 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  var _ = require('../../lodash');
+  var result = _.remove(collection_a, function(n) { return n % collnection_b == 0; });
+  return result;
 }
 
 module.exports = choose_divisible_integer;
--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,9 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var _ = require('../../lodash');
+  var result = _.flattenDeep(collection);
+  return result;
 }
 
 module.exports = double_to_one;

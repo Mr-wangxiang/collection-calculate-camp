a53179e0d81f7acec5acd6d6df290d3af93698de
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-25 13:37:44 +0000

    add  practices/collectionOperator/get_intersection.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -3,7 +3,7 @@
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
   var _ = require('../../lodash');
-  var result = _.intersection(collection_a, collection_b);
+  var result = _.intersection(collection_b, collection_a);
   return result;
 }
 
